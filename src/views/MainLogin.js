d38f341bec8f50963398d0525669b2c469a834ed
JG <iiop25@naver.com>
2023-05-03 17:47:18 +0900

    jg

--- src/views/MainLogin.js
+++ src/views/MainLogin.js
@@ -1,13 +1,16 @@
 // import Login from '../components/login/Login';
 import React, { useState, useEffect } from 'react';
-import AppRouter from '../components/Router';
+import AppRouter from "../components/login/Router";
 import { authService } from '../Loginbase';
-import Auth from '../components/Auth';
+import Auth from '../components/login/Auth';
+// import Auth from "../components/login/Auth";
+// import Auth from "../components/login/Auth"
+// import "../components/login/Login.css";
 
 // npm install firebase 설치 필요
 
-function MainLogin() {
-  const [init, setInit] = useState(false);
+function MainLogin () {
+  const [init,setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
@@ -16,18 +19,76 @@ function MainLogin() {
       } else {
         setIsLoggedIn(false);
       }
-      setInit(true);
-    });
-  }, []);
-
-  return (
-    <>
+        setInit(true);
+      });
+    }, []);
+ 
+    return (
+      <>
       {/* <Auth /> */}
       {/* <AppRouter isLoggedIn={isLoggedIn} /> */}
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initializing...'}
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      {/* {init ? <AppRouter /> : "Initializing..."} */}
       {/* <footer>&copy; {new Date().getFullYear()} asdas</footer> */}
-    </>
-  );
-}
+      </>
+    );
+  }
+
 
 export default MainLogin;
+
+// function MainLogin () {
+//   console.log();
+//   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+//   return (
+//     <>
+//       <AppRouter isLoggedIn={isLoggedIn} />
+//       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+//     </>
+//   );
+// }
+
+
+// export default MainLogin;
+
+// 새로운거 작업
+
+// import React, { useState, useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { loginUser } from '../components/Reducer/userSlice';
+// import axios from 'axios';
+
+// const LoginComponent = () => {
+//   const dispatch = useDispatch();
+  
+//   const [id, setId] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const [loading, setLoading] = useState(false);
+//   const [msg, setMsg] = useState("");
+
+//   useEffect(() = (e) => {
+
+//   }, [msg])
+
+//   const LoginFunc = (e) => {
+//     e.preventDefault();
+
+//   }
+//   return (
+//     <>
+//       <h1>LoginComponent</h1>
+//       <form onSubmit={LoginFunc}>
+//         <label htmlFor="id">ID :</label>
+//         <input type="text" id="id" />
+//         <br />
+//         <label htmlFor="password">Password</label>
+//         <input type="password" />
+//         <br />
+//         <button type="submit">로그인</button>
+//         <br />
+//         {msg}
+//       </form>
+//     </>
+//   )
+// }

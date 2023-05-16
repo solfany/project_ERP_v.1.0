1a59d9fd082b3e0ad88f70595520b3ebe35fc52d
junyihong <junyihong@naver.com>
2023-05-16 16:21:18 +0900

    병합

--- src/Loginbase.js
+++ src/Loginbase.js
@@ -13,12 +13,13 @@ import { getAuth } from 'firebase/auth';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYV8YrdUjqD2HS8vmZJ_-KAEWMpjuOZng",
-  authDomain: "project02-44a94.firebaseapp.com",
-  projectId: "project02-44a94",
-  storageBucket: "project02-44a94.appspot.com",
-  messagingSenderId: "231391389225",
-  appId: "1:231391389225:web:cd72aa1c6681a9090ede38"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
